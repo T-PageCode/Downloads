4e36b4f0d1ac185699e680c8229398d64fc85f72
T-PageCode Developer <cdt20260@outlook.com>
2026-08-28 11:38:51 +0800

    增加下载IDLIGH2FA(I-Dont-Like-It-GitHub-2FA)的页面

---
+++ IDLIGH2FA/index.js
@@ -0,0 +1,40 @@
+const menu = document.getElementById("menu");
+const info = document.getElementById("info");
+let menuTime = null;
+function copyLink(link) {
+    navigator.clipboard.writeText(link);
+    info.style.visibility = "visible";
+    info.style.opacity = "1";
+    menu.style.opacity = "0";
+    menu.style.transform = "translate(-50%,-50%) scale(0.8)";
+    setTimeout(() => {
+        menu.style.visibility = "hidden";
+    },500)
+    setTimeout(() => {
+        info.style.opacity = "0";
+        setTimeout(() => {
+            info.style.visibility = "hidden";
+        },500)
+    },2000)
+}
+document.addEventListener("contextmenu",(e) => {
+    clearTimeout(menuTime)
+    e.preventDefault();
+    let x = e.clientX;
+    let y = e.clientY;
+    menu.style.left = x + "px";
+    menu.style.top = y + "px";
+    menu.style.visibility = "visible";
+    menu.style.opacity = "1";
+    menu.style.transform = "translate(-50%,-50%) scale(1)";
+})
+document.addEventListener("click",(e) => {
+    if (e.target.closest("#menu")) {
+        return;
+    }
+    menu.style.opacity = "0";
+    menu.style.transform = "translate(-50%,-50%) scale(0.8)";
+    menuTime = setTimeout(() => {
+        menu.style.visibility = "hidden";
+    },500)
+})
